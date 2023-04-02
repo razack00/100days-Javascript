9bb7bd68553df0e12b1da81d261e11aef7082bd8
razack00 <121952296+razack00@users.noreply.github.com>
2023-04-02 21:56:24 +0100

    variables

--- variables.js
+++ variables.js
@@ -1,6 +1,31 @@
-var string = "hello"
-var boolean = true
-var nothing 
-var empty = ""
 
-console.log(typeof string) 
+// Declearing four variable and assigning type of (string, boolean, undefined, null)
+var string = "string"
+var boolean = true
+var nothing
+var empty = null
+
+// Declear four variables without assigning values
+var a
+var b 
+var c
+var d
+
+// Declear four variables with assigned values
+
+var day1 = "monday"
+var day2 = "tuesday"
+var day3 = "wednesday"
+var day4 = "thursday"
+
+
+// Declear variables to store your first name, last name, marital status, country and age in a multiple lines
+
+var firstName = "John"
+var lastName = "Peter"
+var maritalStatus = "married"
+var country = "Namibia"
+var age = 30
+
+
+
