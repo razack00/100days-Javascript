0d9994d986a99bc948a1102493f0a1ca4936f983
razack00 <121952296+razack00@users.noreply.github.com>
2023-04-03 23:53:41 +0100

    non-primitive values

--- datatypes.js
+++ datatypes.js
@@ -9,3 +9,15 @@ var floatNum = 5.86643
 
 console.log(typeof string, boolean, nothing, empty, nums, floatNum)
 
+//non-primitive datatypes in javascript include: objects and arrays
+//non-primitive datatypes are modifiable and mutable. their values can be changed
+
+//An array is a list of values in a square bracket
+
+
+// even if non-primitive data types have the same values, they are not strictly equal. example:
+let nums = [2,3,4]
+let numbers = [2,3,4]
+
+console.log(nums == numbers)
+
