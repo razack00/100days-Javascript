8841f965c3d425efceb658d958e8049944983e63
razack00 <121952296+razack00@users.noreply.github.com>
2023-04-12 14:22:23 +0100

    more on datatypes

--- datatypes.js
+++ datatypes.js
@@ -16,10 +16,35 @@ console.log(typeof string, boolean, nothing, empty, nums, floatNum)
 
 
 // even if non-primitive data types have the same values, they are not strictly equal. example:
-let nums = [2,3,4]
+let numb = [2,3,4]
 let numbers = [2,3,4]
 
 console.log(nums == numbers)
+
+const myArray = [ 34, 534,343,2,34,5]
+
+
+// console.log(myArray[0])
+
+// an array for 5 years of birth
+
+let yearsOfBirth = []
+
+yearsOfBirth.push(1994)
+yearsOfBirth.push(1995)
+yearsOfBirth.push(1996)
+yearsOfBirth.push(1997)
+yearsOfBirth.push(1998)
+
+console.log('year one', 2023 - yearsOfBirth[0] )
+console.log('year two', 2023 - yearsOfBirth[1] )
+console.log('year three', 2023 - yearsOfBirth[2] )
+console.log('year four', 2023 - yearsOfBirth[3] )
+console.log('year five', 2023 - yearsOfBirth[4] )
+
+
+
+
 
 //objects
 
