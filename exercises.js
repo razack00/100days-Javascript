16a5168735a3e68dc518fede43f30e20692b9ef4
razack00 <121952296+razack00@users.noreply.github.com>
2023-04-12 11:59:29 +0100

    exercise

---
+++ exercises.js
@@ -0,0 +1,59 @@
+
+// Exercise Day2
+const { is } = require("express/lib/request")
+
+var firstName = "John"
+var lastName = "Peter"
+var country = "cameroon"
+var city = "Yaounde"
+var isMarried = true
+var year = "2023"
+
+console.log(typeof firstName, lastName, country, city, isMarried, year )
+
+// return a boolean
+var equal = 10 == 10
+
+console.log(equal)
+
+var equalTo = 4
+
+var checkParseInt = parseInt(7,8)
+var num = 7
+console.log("hello" , checkParseInt == num)
+
+console.log(parseInt(9.8) == 10)
+
+
+
+// // Exercise Day 3
+// var h = "hello, your, at"
+// console.log(h.split(','))
+
+
+var changeString = '30 days of javascript'
+
+console.log(changeString.replace("javascript", "python"))
+
+// checkEmail("hello@gmail.com")
+
+let email = "hello@gmail.com"
+email = email.trim()
+let emailIsValid =(email.includes(".") && email.includes("@"))
+
+console.log(emailIsValid && "email is valid")
+console.log(!emailIsValid && "email is not valid")
+
+
+// email validation
+function checkEmail(email) {
+    email.trim()
+    if (email.includes('@') && email.includes(".com")) {
+        console.log("hello there email valid")
+    }
+    else {
+        console.log("not valid")
+    }
+}
+
+checkEmail("razack@gmail.com")
