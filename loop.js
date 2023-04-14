ffc85dbe7f0003d6590a629182a30ce9c86bc26c
razack00 <121952296+razack00@users.noreply.github.com>
2023-04-14 19:49:11 +0100

    intro to loops

--- loop.js
+++ loop.js
@@ -14,3 +14,4 @@ for (let counter = 0; counter < yearsOfBirth.length; counter ++) {
     isEligible = age >= 21;
     console.log(`you are ${age} year old since you are bor in ${yearsOfBirth[counter]} and ${isEligible ? 'you can vote' : 'you cannot vote'} `)
 }
+
