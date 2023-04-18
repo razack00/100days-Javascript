a6f2ac38d4b1f4f53382903100917f5789e11225
razack00 <121952296+razack00@users.noreply.github.com>
2023-04-18 23:30:37 +0100

    solved some problems

--- Day3_exercises.js
+++ Day3_exercises.js
@@ -110,11 +110,56 @@ console.log((new Date().getTime()))
 
 // Exercises: Level 2
 
-// Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+// 1. Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+
+// Enter base: 20
+// Enter height: 10
+// The area of the triangle is 100
 
 function promptMe() {
     let base = prompt('enter base')
     let height = prompt('enter height')
     let area = 0.5 * base * height
-    document.querySelector('h1').innerText = area
+    document.querySelector('h1').innerText = `The areaaa of the triangle ${area} `
 }
+
+// 1. Write a script that prompt the user to enter side a, side b, and side c of the triangle and andcalculate the perimeter of triangle (perimeter = a + b + c)
+
+// Enter side a: 5
+// Enter side b: 4
+// Enter side c: 3
+// The perimeter of the triangle is 12
+
+function triangle() {
+    let a = parseInt(prompt("Enter side a: "))
+    let b = parseInt(prompt("Enter side b: "))
+    let c = parseInt(prompt("Enter side c: "))
+    let perimeter = a + b + c
+    document.querySelector('h3').innerText = `The perimeter of the triangle is ${perimeter}` 
+}
+
+// 1. Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+function rectangle() {
+    let length = parseInt(prompt("Enter side length: "))
+    let width = parseInt(prompt("Enter side width: "))
+    let area = length * width
+    let perimeter = 2 * (length + width)
+    document.querySelector('.rectangle').innerText = `The area of the rectangle is ${area} \n The perimeter of the rectangle is ${perimeter}` 
+}
+
+// 2.Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+function circle() {
+    let radius = parseInt(prompt("Enter radius of  circle: "))
+    let pi = 3.14
+    let area = pi * radius * radius
+    let circumference = 2 * pi * radius
+    document.querySelector('.rectangle').innerText = `The area of the circle is ${area} \n The circumference of the circle is ${circumference}` 
+}
+// 3. Calculate the slope, x-intercept and y-intercept of y = 2x -2
+
+
+// 4. Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+// 5. Compare the slope of above two questions.
+// 6. Calculate the value of y (y = x + 6x + 9). Try to use different x values and figure out at what x value y is 0. 
+// 7. Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
+
