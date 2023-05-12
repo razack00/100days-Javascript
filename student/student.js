95bc4998f8ce5fb4de3066be45ccd79d4b8cfb46
razack00 <121952296+razack00@users.noreply.github.com>
2023-05-12 12:04:23 +0100

    update to student

--- student/student.js
+++ student/student.js
@@ -1,9 +1,10 @@
 const student = {
     matricule: '123456',
     name: {
-        firstName: 'john',
+        firstName: 'John',
         lastName: 'Doe',
     },
+    image: 'ps5.webp',
     courses: [
         {
             courseNo: 'INFO-1010',
@@ -61,6 +62,42 @@ const student = {
     gender: 'Male',
 }
 
-console.log(`Name: ${student.name.firstName} ${student.name.lastName},\nQuater: ${student.address.quarter}, \nGender: ${student.gender}`)
 
-console.table(student.courses[0])
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.profile-img').src = student.image
+    document.querySelector('.name').textContent =`${student.name.firstName} ${student.name.lastName}`
+    document.querySelector('.matricule').textContent = `${student.matricule}`
+    document.querySelector('.gender').textContent = `${student.gender}`
+    document.querySelector('.quater').textContent = `${student.address.quarter}`
+
+    // const course = document.querySelector('.course-name').textContent = `${student.courses[0].courseName}`
+
+    const container = document.querySelector('.container')
+
+    const courses = student.courses
+
+    let content = courses.map((course) => {
+        return `<div class="course-info-container">
+            <p>COURSE: <span class="course-name"> ${course.courseName} </span></p>
+            <ul class="course-info">
+            <div>
+                <h5>COEF</h5>
+                <li class="">${course.credits}</li>
+            </div>
+            <div>
+                <h5>SCORE</h5>
+                <li class="score">${course.score}</li>
+            </div>
+            <div>
+                <h5>TEACHER</h5>
+                <li class="teacher">${course.teacher.name.firstName} ${course.teacher.name.lastName}</li>
+            </div>
+            </ul> 
+        </div>`
+    } ) 
+    container.innerHTML = content
+})
+
+
+ 
+
